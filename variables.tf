d461081f1ad006d3876be7af70558ca5b8f47bdd
gayucharvi <153450955+gayucharvi@users.noreply.github.com>
2023-12-21 16:24:06 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,36 @@
+variable "vpc_cidr" {
+  description = "VPC CIDR BLOCK"
+  default = "10.0.0.0/16"
+}
+
+variable "private_subnet_cidr" {
+  description = "PRIVATE SUBNET CIDR"
+  default = "10.0.1.0/24"
+}
+
+variable "public_subnet_cidr" {
+  description = "PUBLIC SUBNET CIDR"
+  default = "10.0.0.0/24"
+}
+
+variable "region_location" {
+  description = "selected region"
+ }
+
+variable "aws_root_account_id" {
+  default = "xx"
+}
+
+variable "terraform_user_arn" {
+  default = "xx"
+}
+
+variable "username" {
+  default = "terraform-uat"
+}
+
+
+
+
+
+
