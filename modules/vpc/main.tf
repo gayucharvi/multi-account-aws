1fdc5449b673869c2969152eae8e97a7a3d09c77
gayucharvi <153450955+gayucharvi@users.noreply.github.com>
2023-12-22 12:14:34 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,32 +1,30 @@
 # Create a VPC
-resource "aws_vpc" "example" {
+resource "aws_vpc" "myvpc" {
   cidr_block = var.vpc_cidr
-
+  enable_dns_hostnames = true
   tags = {
-    Name = "vpcnew"
+    Name = "myvpc"
   }
 }
 
-resource "aws_subnet" "private" {
-  vpc_id     = aws_vpc.example.id
+resource "aws_subnet" "private_subnet" {
+  count = "${length(data.aws_availability_zones.available.names)}"
+  vpc_id     = aws_vpc.myvpc.id
   cidr_block = var.private_subnet_cidr
-  availability_zone="ap-south-1a"
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-
-  tags = {
-    Name = "subnet_private"
+  availability_zone = "${data.aws_availability_zones.available.names[count.index]}"
+  map_public_ip_on_launch = false
+  tags {
+    Name = "PrivateSubnet"
   }
 }
 
-resource "aws_subnet" "public" {
-  vpc_id     = aws_vpc.example.id
+resource "aws_subnet" "public_subnet" {
+  count = "${length(data.aws_availability_zones.available.names)}"
+  vpc_id     = aws_vpc.myvpc.id
   cidr_block = var.public_subnet_cidr
-  availability_zone="ap-south-1a"
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-
-  tags = {
-    Name = "subnet_public"
+  availability_zone= "${data.aws_availability_zones.available.names[count.index]}"
+   map_public_ip_on_launch = true
+  tags {
+    Name = "PublicSubnet"
   }
 }
