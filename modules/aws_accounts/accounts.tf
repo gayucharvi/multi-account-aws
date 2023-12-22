44f146a84f6f09140a3399a80fa9f5d570df8b93
gayucharvi <153450955+gayucharvi@users.noreply.github.com>
2023-12-22 14:04:55 +0530

    Update accounts.tf

--- modules/aws_accounts/accounts.tf
+++ modules/aws_accounts/accounts.tf
@@ -9,11 +9,6 @@ resource "aws_organizations_account" "dev" {
   }
 }
 
-resource "aws_iam_account_alias" "alias" {
-  provider      = aws.sub-account
-  account_alias = var.account_name
-}
-
 output "aws_account_ids" {
   value = {
     dev = aws_organizations_account.dev.id
