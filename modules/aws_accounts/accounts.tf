cef915292a323b695b4aab18f7306fa29a9bd2db
gayucharvi <153450955+gayucharvi@users.noreply.github.com>
2023-12-22 13:59:48 +0530

    Update accounts.tf

--- modules/aws_accounts/accounts.tf
+++ modules/aws_accounts/accounts.tf
@@ -1,13 +1,17 @@
 
 
 resource "aws_organizations_account" "dev" {
-
-  name = "dev"
-  email = "dev@example99999.com"
-
+  name = var.account_name
+  email = var.email_address
+  role_name = "owner"
   lifecycle {
     prevent_destroy = true
   }
+}
+
+resource "aws_iam_account_alias" "alias" {
+  provider      = aws.sub-account
+  account_alias = var.account_name
 }
 
 output "aws_account_ids" {
