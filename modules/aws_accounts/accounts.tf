1fbe5412b5c976765888ed2f1459095adeab9957
gayucharvi <153450955+gayucharvi@users.noreply.github.com>
2023-12-22 13:04:32 +0530

    Update accounts.tf

--- modules/aws_accounts/accounts.tf
+++ modules/aws_accounts/accounts.tf
@@ -1,1 +1,17 @@
 
+
+resource "aws_organizations_account" "dev" {
+
+  name = "dev"
+  email = "dev@example99999.com"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+output "aws_account_ids" {
+  value = {
+    dev = aws_organizations_account.dev.id
+  }
+}
